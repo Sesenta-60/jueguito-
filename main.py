a06323be59bbbe5efbf054b342be215f71a3cc14
Sesenta <jonyta800@gmail.com>
2025-12-06 22:20:49 +0000

    empezo

---
+++ main.py
@@ -0,0 +1,5 @@
+import objetos
+
+usuario=objetos.personaje("jonathan",10,10,{"fuerza":5,"destreza":7,"inteligencia":6})
+
+usuario.mostrar()
---
+++ objetos.py
@@ -0,0 +1,50 @@
+class personaje:
+    def __init__(self,nombre,vidam,manam,estadisticas,puntos=0):
+        self.nombre=nombre
+        self.vida=vidam
+        self.mana=manam
+        self.estadisticas=estadisticas
+        self.daño=self.estadisticas["fuerza"] * 2
+        self.vida_actual=self.vida
+        self.mana_actual=self.mana
+        self.puntos_de_experiencia=puntos
+
+    def mostrar(self):
+        print(f"Nombre: {self.nombre}")
+        print(f"Vida: {self.vida}")
+        print(f"Mana: {self.mana}")
+        print("Estadísticas:")
+        for key, value in self.estadisticas.items():
+            print(f"  {key}: {value}")
+    
+    def mostrar_estadisticas(self):
+        for key, value in self.estadisticas.items():
+            print(f"  {key}: {value}")
+    
+    def mostrar_nombre(self):
+        print(f"Nombre: {self.nombre} | Vida: {self.vida_actual} | Mana: {self.mana_actual}")
+
+    def atacar(self,objetivo):
+        print(f"{self.nombre} ataca a {objetivo.nombre}!")
+        objetivo.vida_actual -= self.daño
+
+    def mejorar(self):
+        self.mostrar_estadisticas()
+        stat=input("que estadisticca quieres mejorar?")
+        if stat not in self.estadisticas:
+            print("estadistica no valida")
+            return
+        cantidad=input("cuantos puntos quieres invertir?")
+        try:
+            cantidad=int(cantidad)
+        except ValueError:
+            print("cantidad no valida")
+            return
+        if self.puntos_de_experiencia < cantidad:
+            print(f"No tienes suficientes puntos de experiencia para mejorar {stat}.")
+            return
+        if stat in self.estadisticas:
+            self.estadisticas[stat] += cantidad
+            print(f"{self.nombre} ha mejorado {stat} en {cantidad} puntos.")
+        else:
+            print(f"{stat} no es una estadística válida.")
